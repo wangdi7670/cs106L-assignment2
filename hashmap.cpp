360b11afae96d01d60399e3b93143fd4222b506d
WangDi <wangdi7670@163.com>
2023-10-19 14:05:10 +0000

    modify size(), empty(), contains(), load_factor(), find() to const-interface

--- hashmap.cpp
+++ hashmap.cpp
@@ -21,17 +21,17 @@ HashMap<K, M, H>::~HashMap() {
 }
 
 template <typename K, typename M, typename H>
-inline size_t HashMap<K, M, H>::size() {
+inline size_t HashMap<K, M, H>::size() const {
     return _size;
 }
 
 template <typename K, typename M, typename H>
-inline bool HashMap<K, M, H>::empty() {
+inline bool HashMap<K, M, H>::empty() const {
     return size() == 0;
 }
 
 template <typename K, typename M, typename H>
-inline float HashMap<K, M, H>::load_factor() {
+inline float HashMap<K, M, H>::load_factor() const {
     return static_cast<float>(size())/bucket_count();
 };
 
@@ -50,7 +50,7 @@ M& HashMap<K, M, H>::at(const K& key) const {
 }
 
 template <typename K, typename M, typename H>
-bool HashMap<K, M, H>::contains(const K& key) {
+bool HashMap<K, M, H>::contains(const K& key) const {
     return find_node(key).second != nullptr;
 }
 
@@ -68,6 +68,13 @@ template <typename K, typename M, typename H>
 typename HashMap<K, M, H>::iterator HashMap<K, M, H>::find(const K& key) {
     return make_iterator(find_node(key).second);
 }
+
+
+template <typename K, typename M, typename H>
+typename HashMap<K, M, H>::const_iterator HashMap<K, M, H>::find(const K& key) const{
+    return static_cast<const_iterator>(const_cast<HashMap<K, M, H>*>(this)->find(key));
+}
+
 
 template <typename K, typename M, typename H>
 std::pair<typename HashMap<K, M, H>::iterator, bool> HashMap<K, M, H>::insert(const value_type& value) {
