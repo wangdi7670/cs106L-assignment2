02505dec9e9ad039a6a9d04cce4a9e0631180ba7
WangDi <wangdi7670@163.com>
2023-10-23 12:45:17 +0000

    deal with self-assignment

--- hashmap.cpp
+++ hashmap.cpp
@@ -37,6 +37,9 @@ HashMap<K, M, H>::HashMap(const HashMap& other) :
 template <typename K, typename M, typename H>
 HashMap<K, M, H>& HashMap<K, M, H>::operator=(const HashMap& other)
 {
+    if (this == &other) {
+        return *this;
+    }
     _size = 0;
     _hash_function = other._hash_function;
     for (size_t i = 0; i < other.size(); i++) {
